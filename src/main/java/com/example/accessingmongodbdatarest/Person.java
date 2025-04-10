c8b38d5eaf1e15bfed6dec8c3cc68c4ecf0b0ea0
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-04-10 21:12:48 +0200

    Commit

--- src/main/java/com/example/accessingmongodbdatarest/Person.java
+++ src/main/java/com/example/accessingmongodbdatarest/Person.java
@@ -1,7 +1,9 @@
 package com.example.accessingmongodbdatarest;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "people")
 public class Person {
     @Id
     public String id;
